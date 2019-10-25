1e6dd7a28c54b7feea9f455a93a7fc7e58922c33
anton-serozhechkin <serozhechkintosh@gmail.com>
2019-10-25 17:24:15 +0300

    update requirements, add func in views

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.main),
+    re_path('(?P<category>\d+)', views.show_categ),
+    #re_path('(?P<category>&<id>\d+)', views.show_article)
+]
--- analytics/views.py
+++ analytics/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from models import Category
+from .models import *
 
 def main(request):
-    data = Category.objects.all()
-    return data
+    context = []
+    data_cat = Category.objects.all()
+    data_art = Article.objects.all()
+    context.append({'data_cat': data_cat, 'data_art': data_art})
+    print(context)
+    return context
 
+def show_categ(request):
+    data_cat = Category.objects.filter(is_active=True)
+    context = []
+    context.append({'data_cat': data_cat})
+    print(context)
+    return context
