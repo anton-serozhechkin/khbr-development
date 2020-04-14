535269129d6cdc88c2ca5af9dec54e786bb9f945
Apppie <dbabichwork@gmail.com>
2020-04-14 02:47:28 +0200

    Added media styles ad header section to all default mobile devices

--- static/js/media.js
+++ static/js/media.js
@@ -1,18 +1,26 @@
-let videoArray = document.querySelectorAll("iframe");
-let followButton = document.querySelectorAll('.index-follow-button');
+let navBars = document.getElementById('header-navbars-id');
+let headerNavList = document.getElementsByClassName('header-navigation-item');
+let headerNav = document.getElementById('header-anchor');
+let headerNavMenu = document.getElementById('header-navigation-id');
 
-if (screen.width <= 375) {
-    for (let i = 0; i < videoArray.length; i++) {
-       videoArray[i].width = '100%';
+if (screen.width <= 480) {
+    navBars.style.display = "block";
+    for(let i = 0; i < headerNavList.length;i++) {
+        headerNavList[i].classList.add('navUnHide')
     }
+    navBars.classList.add('hide');
+    navBars.addEventListener('click',function () {
+        for(let i = 0; i < headerNavList.length;i++) {
+            headerNavList[i].classList.remove('navUnHide');
+            headerNavList[i].classList.add('wow');
+            headerNavList[i].classList.add('fadeIn');
+        }
+        headerNavMenu.classList.add('navMediaMenu');
+        navBars.addEventListener('click',function () {
+            for(let i = 0; i < headerNavList.length;i++) {
+                headerNavList[i].classList.add('navUnHide');
+            }
+        })
+    });
 }
-if (screen.width <= 414) {
-    for (let i = 0; i < videoArray.length; i++) {
-       videoArray[i].width = '100%';
-    }
-}
-if (screen.width <= 768) {
-    for (let i = 0; i < videoArray.length; i++) {
-       videoArray[i].width = '100%';
-    }
-}
+
