6b71df318e44263353def282e3c990d9e16c71da
anton-serozhechkin <serezhechkintosh@gmail.com>
2020-06-19 20:14:47 +0300

    change form signin and signup

--- khbr/urls.py
+++ khbr/urls.py
@@ -1,18 +1,3 @@
-"""khbr URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, reverse
 from django.conf import settings
@@ -62,7 +47,7 @@ urlpatterns = [
     path('events/', include('event.urls')),
     path('videooverviews/', include('videooverview.urls')),
     path('raitings/', include('raitings.urls')),
-    #path('user/', include('users.urls')),
+    path('user/', include('users.urls')),
     path('poll/', include('poll.urls')),
     path('', include(tf_urls, "two_factor")),
     path('accounts/', include('allauth.urls')),
--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,25 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин')
+    email = forms.CharField(label='Электронная почта')
+    password = forms.CharField(label='Пароль', widget=forms.HiddenInput())
+
     class Meta:
         model = User
         fields = ['username', 'email',
                 'password']
 
-class SignInUserForm(UserCreationForm):
+
+class SignInUserForm(ModelForm):
+    
+    username = forms.CharField(label='Логин', max_length=100)
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ['username', 'password']
