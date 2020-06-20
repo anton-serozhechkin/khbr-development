9385c1cfb85e520c09c917de1b9949d574826b86
anton-serozhechkin <serezhechkintosh@gmail.com>
2020-06-20 23:56:10 +0300

    add cherror on sign templates, views, check if user auth redirect on analytics

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from .forms import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import auth
 from analytics.models import Subscribe
+from django.contrib.auth.models import User
 
 def logout_view(request):
     logout(request)
@@ -11,43 +12,57 @@ def logout_view(request):
 def recovery(request):
     if request.method == 'POST':
         email = request.POST['email']
-        print(email)
     return render(request, 'user/recovery.html', locals())
 
 def signin(request):
-    if request.method == 'POST':
-        
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('analytics')
-        else:
-            return redirect('signin')
+    if request.user.is_authenticated:
+        return redirect('analytics')
+    else:
+        if request.method == 'POST':
+
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('analytics')
+            else:
+                error = 'Неверный логин или пароль.'
+
     return render(request, 'user/signin.html', locals())
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False) 
-            user.email = form.cleaned_data.get('email')
-            user.username = form.cleaned_data.get('username')
-            user.password1 = form.cleaned_data.get('password1')
-            user.password2 = form.cleaned_data.get('password2')
-            try:
-                subscribe = request.POST['subscribe']
-                new_subscriber = Subscribe.objects.create(email=user.email)
-                new_subscriber.save()
-            except:
-                pass
+    if request.user.is_authenticated:
+        return redirect('analytics')
+    else:
+        if request.method == 'POST':
+            form = SignUpUserForm(request.POST)
+            if form.is_valid():
+
+                user = form.save(commit=False) 
+                user.email = form.cleaned_data.get('email')
+                user.username = form.cleaned_data.get('username')
+                user.password1 = form.cleaned_data.get('password1')
+                user.password2 = form.cleaned_data.get('password2')
+
+                try:
+                    subscribe = request.POST['subscribe']
+                    #встаивть проверку существует ли эмейл
+                    new_subscriber = Subscribe.objects.create(email=user.email)
+                    new_subscriber.save()
+                except:
+                    pass
+                
+                form.save()
+                login(request, user, backend='django.contrib.auth.backends.ModelBackend')            
+                return redirect('analytics')
             
-            form.save()
-            login(request, user, backend='django.contrib.auth.backends.ModelBackend')            
-            return redirect('analytics')
+            else:
+                error = 'Что-то пошло не так, проверьте ваши данные'
+                form = SignUpUserForm()
         else:
             form = SignUpUserForm()
-    else:
-        form = SignUpUserForm()
+
     return render(request, 'user/signup.html', locals())
