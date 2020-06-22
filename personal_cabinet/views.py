b49fc37b3bc7345bdced60bd94cd3a9a5b0b9be3
anton-serozhechkin <serezhechkintosh@gmail.com>
2020-06-22 08:52:58 +0300

    add news, raitings, events on personal cabinet, add transfer to other links on template

--- personal_cabinet/views.py
+++ personal_cabinet/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from analytics.models import Subscribe
-
+from analytics.models import Subscribe, Article
+from event.models import Event
+from raitings.models import Raiting
 @login_required(login_url='/user/signin/')
 def personal_cabinet(request, private_data=None, change_password=None, unsubscribe=None, subscribe=None, links=None, delete_account=None):
     if Subscribe.objects.filter(email=request.user.email).exists():
         already_subscriber = True
+    
+    last_articles = Article.objects.filter(is_active=True)[0:3]
+    last_events = Event.objects.filter(is_active=True)[0:2]
+    last_raitings = Raiting.objects.filter(is_active=True)[0:1]
+
+    fuck = True
     if str(request.build_absolute_uri).rsplit('/', 1)[-1] == "private_data'>>":
-        print('da')
-    print(str(request.build_absolute_uri).rsplit('/', 1)[-1])
+        private_data = True
+        fuck = False
+    
+    if str(request.build_absolute_uri).rsplit('/', 1)[-1] == "change_password'>>":
+        change_password = True
+        fuck = False
+    
+    if str(request.build_absolute_uri).rsplit('/', 1)[-1] == "unsubscribe'>>":
+        unsubscribe = True
+        fuck = False
+
+    if str(request.build_absolute_uri).rsplit('/', 1)[-1] == "subscribe'>>":
+        subscribe = True
+        fuck = False
+
+    if str(request.build_absolute_uri).rsplit('/', 1)[-1] == "links'>>":
+        links = True
+        fuck = False
+
+    if str(request.build_absolute_uri).rsplit('/', 1)[-1] == "delete_account'>>":
+        delete_account = True
+        fuck = False
+
     return render(request, 'personal_cabinet/index.html', locals())
