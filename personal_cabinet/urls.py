698b349fb4a3e0394a0c49495529522bf46e0bd8
anton-serozhechkin <serezhechkintosh@gmail.com>
2020-07-14 14:52:16 +0300

    readd normal urls and their handlers in personal cabinet

--- personal_cabinet/urls.py
+++ personal_cabinet/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.personal_cabinet, name='personal_cabinet'),
-    re_path('private_data', views.personal_cabinet, name='private_data'),
-    re_path('change_password', views.personal_cabinet, name='change_password'),
-    re_path('subscribe', views.personal_cabinet, name='subscribe'),
-    re_path('unsubscribe', views.personal_cabinet, name='unsubscribe'),
-    re_path('links', views.personal_cabinet, name='links'),
+    path('', views.personal_cabinet, {'news': True}, name='personal_cabinet'),
+    re_path('&private_data/$', views.personal_cabinet, {'private_data': True}, name='private_data'),
+    re_path('&change_password/$', views.personal_cabinet, {'change_password': True}, name='change_password'),
+    re_path('&subscribe/$', views.personal_cabinet, {'subscribe': True}, name='subscribe'),
+    re_path('&unsubscribe/$', views.personal_cabinet, {'unsubscribe': True}, name='unsubscribe'),
+    re_path('&links/$', views.personal_cabinet, {'links': True}, name='links'),
 ]
